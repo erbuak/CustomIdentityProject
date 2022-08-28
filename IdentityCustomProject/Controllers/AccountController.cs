338b67666fa3250c7928c823bd901145bc70d6e7
Erkut Akyaz <erkutakyaz@gmail.com>
2022-08-28 23:16:14 +0300

    Email Onay Linkinin Oluşturulması ve Email Onayı

--- IdentityCustomProject/Controllers/AccountController.cs
+++ IdentityCustomProject/Controllers/AccountController.cs
@@ -43,8 +43,19 @@ namespace IdentityCustomProject.Controllers
 
                     if (result.Succeeded)
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return RedirectToAction("Index", "home");
+                        var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                        var emailConfirmationLink = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, token = token }, Request.Scheme);
+
+                        ViewBag.SuccessMessage = "Registration succesful. Before try to login, please confirm your email" +
+                            " by clicking on the confirmation link we have emailed you.";
+
+                        Console.Write(emailConfirmationLink);
+
+                        //await _signInManager.SignInAsync(user, isPersistent: false);
+                        //return RedirectToAction("Index", "home");
+
+                        return View();
                     }
 
                     foreach (var error in result.Errors)
@@ -76,7 +87,7 @@ namespace IdentityCustomProject.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "home");
+                    return RedirectToAction("Index", "Home");
                 }
 
                 ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
@@ -88,7 +99,36 @@ namespace IdentityCustomProject.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("index", "home");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if(userId == null || token == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = "The user id " + userId + " is invalid!";
+                return View();
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+
+            if (result.Succeeded)
+            {
+                ViewBag.SuccessMessage = "Thank you for confirming your email! Now you can be login!";
+                return View();
+            }
+
+            ViewBag.ErrorMessage = "Email cannot be confirmed!";
+
+            return View();
         }
     }
 }
--- IdentityCustomProject/Program.cs
+++ IdentityCustomProject/Program.cs
@@ -1,5 +1,6 @@
 using IdentityCustomProject.Data;
 using IdentityCustomProject.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,9 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContextString")));
 
-builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<AppUser, AppRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
